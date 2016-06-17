b77a471ce15dd966b161c0627d3bc03586225c9f
yoku0825 <yoku0825@gmail.com>
2016-06-17 17:37:00 +0900

    Stop container first when it is running

--- my_gitbucket/start.sh
+++ my_gitbucket/start.sh
@@ -1,1 +1,5 @@
-docker run -d -p 50088:8080 -p 52200:52200 -v ${PWD}/data:/gitbucket --name=gitbucket f99aq8ove/gitbucket
+if docker stop gitbucket > /dev/null 2>&1 ; then
+  docker rm gitbucket > /dev/null
+fi
+
+docker run -d -p 50088:8080 -p 52200:52200 -v ${PWD}/data:/gitbucket --name=gitbucket yoku0825/gitbucket
