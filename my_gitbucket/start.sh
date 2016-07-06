556c1c0e4403f021ea427fec9f1b43098b0150e1
yoku0825 <yoku0825@gmail.com>
2016-07-06 11:29:52 +0900

    Move to yoku0825/my_gitbucket.

--- my_gitbucket/start.sh
+++
@@ -1,5 +0,0 @@
-if docker stop gitbucket > /dev/null 2>&1 ; then
-  docker rm gitbucket > /dev/null
-fi
-
-docker run -d -p 50088:8080 -p 52200:52200 -v ${PWD}/data:/gitbucket --name=gitbucket yoku0825/gitbucket
